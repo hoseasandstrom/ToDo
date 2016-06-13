52d2bf968f8addfb820a2ddadb74c4e9135b2ab4
hoseasandstrom <Lakyn4ever>
2016-06-13 12:15:29 -0400

    Add database

--- src/com/studenttheironyard/Main.java
+++ src/com/studenttheironyard/Main.java
@@ -1,24 +1,54 @@
 package com.studenttheironyard;
 
+import org.h2.tools.Server;
+
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void createItem(Scanner scanner, ArrayList<ToDoItem> items) {
-        System.out.println("Enter your to do item:");
-        String text = scanner.nextLine();
-        ToDoItem item = new ToDoItem(text, false);
-        items.add(item);
+    public static void insertToDo(Connection conn, String text) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("INSERT INTO todos VALUES (Null, ?, FALSE)");
+        stmt.setString(1, text);
+        stmt.execute();
+    }
+    public static ArrayList<ToDoItem> selectToDos(Connection conn) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("SELECT* FROM todos");
+        ResultSet results = stmt.executeQuery();
+        ArrayList<ToDoItem> items = new ArrayList<>();
+        while (results.next()) {
+            int id = results.getInt("id");
+            String text = results.getString("text");
+            boolean isDone = results.getBoolean("is_done");
+            ToDoItem item = new ToDoItem(id, text, isDone);
+            items.add(item);
+        }
+        return items;
     }
 
-    public static void toggleItem(Scanner scanner, ArrayList<ToDoItem> items) {
+    public static void toggleTodo(Connection conn, int id) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("UPDATE todos SET is_done = NOT is_done WHERE id = ?");
+        stmt.setInt(1, id);
+        stmt.execute();
+    }
+
+    public static void createItem(Scanner scanner, Connection conn) throws SQLException {
+        System.out.println("Enter your to do item:");
+        String text = scanner.nextLine();
+        //ToDoItem item = new ToDoItem(text, false);
+        //items.add(item);
+        insertToDo(conn, text);
+    }
+
+    public static void toggleItem(Scanner scanner, Connection conn) throws SQLException {
         System.out.println("Enter the number of the item you wish to toggle:");
         String numStr = scanner.nextLine();
         try {
             int num = Integer.valueOf(numStr);
-            ToDoItem tempItem = items.get(num - 1);
-            tempItem.isDone = !tempItem.isDone;
+            //ToDoItem tempItem = items.get(num - 1);
+            //tempItem.isDone = !tempItem.isDone;
+            toggleTodo(conn, num);
         }
         catch (NumberFormatException e) {
             System.out.println("You didn't type a number!");
@@ -28,21 +58,28 @@ public class Main {
         }
     }
 
-    public static void listItems(ArrayList<ToDoItem> items) {
-        int i = 1;
+    public static void listItems(Connection conn) throws SQLException {
+        //int i = 1;
         // list item
+        ArrayList<ToDoItem> items = selectToDos(conn);
         for (ToDoItem toDoItem : items) {
             String checkbox = "[ ]";
             if (toDoItem.isDone) {
                 checkbox = "[x]";
             }
-            System.out.printf("%s %s. %s\n", checkbox, i , toDoItem.text);
-            i++;
+            //use this format from now on
+            System.out.printf("%s %s. %s\n", checkbox, toDoItem.id , toDoItem.text);
+            //i++;
         }
 
     }
-    public static void main(String[] args) {
-        ArrayList<ToDoItem> items = new ArrayList<>();
+    public static void main(String[] args) throws SQLException {
+        Server.createWebServer().start();
+        Connection conn = DriverManager.getConnection("jdbc:h2:./main");
+
+        Statement stmt = conn.createStatement();
+        stmt.execute("CREATE TABLE IF NOT EXISTS todos (id IDENTITY, text VARCHAR, is_done BOOLEAN)");
+
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
@@ -54,16 +91,16 @@ public class Main {
             switch (option) {
                 case "1":
                     //create new item
-                    createItem(scanner, items);
+                    createItem(scanner, conn);
                     break;
 
                 case "2":
                     //toggle item
-                    toggleItem(scanner, items);
+                    toggleItem(scanner, conn);
                     break;
 
                 case "3":
-                   listItems(items);
+                   listItems(conn);
                     break;
 
                 default:
--- src/com/studenttheironyard/ToDoItem.java
+++ src/com/studenttheironyard/ToDoItem.java
@@ -4,12 +4,14 @@ package com.studenttheironyard;
  * Created by hoseasandstrom on 5/23/16.
  */
 public class ToDoItem {
+    int id;
     String text;
     boolean isDone;
 
-    public ToDoItem(String text, boolean isDone) {
+
+    public ToDoItem(int id, String text, boolean isDone) {
+        this.id = id;
         this.text = text;
         this.isDone = isDone;
     }
-
 }
